ebe8b667962e27b556d9b28d65267c8edcbe794e
leonardosguimaraes <silver20rj@gmail.com>
2022-09-28 15:38:29 -0300

    Dev Club Convert Money

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 
-const dolar = 5.38
-const euro = 5.19
+const dolar = 5.35
+const euro = 5.21
 
 const convertValues = () => {
     const inputReais = document.getElementById("input-real").value
