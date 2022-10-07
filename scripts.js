90c1b732ba033a8db2440bbd5082419cbd5cb4b8
leonardosguimaraes <silver20rj@gmail.com>
2022-10-07 11:10:27 -0300

    Dev Club Converter

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 
-const dolar = 5.35
-const euro = 5.21
+const dolar = 5.22
+const euro = 5.10
+const bitcoin = 103.000
 
 const convertValues = () => {
     const inputReais = document.getElementById("input-real").value
     const realValueText = document.getElementById("real-value-text")
     const currencyValueText = document.getElementById("currency-value-text")
-
-// realValueText.innerHTML = inputReais
 
     realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -17,17 +16,24 @@ const convertValues = () => {
     }).format(inputReais)
 
     if (select.value === "US$ Dólar americano") {
-        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", { 
-            style: "currency", 
-            currency: "USD" 
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
         }).format(inputReais / dolar)
     }
-    
-    if (select.value === "€ Euro"){
-    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency", 
-        currency: "EUR", 
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
         }).format(inputReais / euro)
+    }
+
+    if (select.value === "₿ Bitcoin") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputReais / bitcoin / 1000)
     }
 }
 
@@ -35,13 +41,18 @@ changeCurrency = () => {
     const currencyName = document.getElementById("currency-name")
     const currencyImg = document.getElementById("currency-img")
 
-    if(select.value === "US$ Dólar americano"){
+    if (select.value === "US$ Dólar americano") {
         currencyName.innerHTML = "Dólar americano"
         currencyImg.src = "./assets/estados-unidos.png"
     }
-    if(select.value === "€ Euro"){
+    if (select.value === "€ Euro") {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "./assets/euro.png"
+    }
+
+    if (select.value === "₿ Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
     }
     convertValues()
 }
